b8155c91caccc2d5d76c08f8a64b850f2c0b1043
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-05-27 12:14:55 -0400

    done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,30 +4,28 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path, alert: "Artist not found"
-      else
-        @songs = @artist.songs
-      end
+      artist_check
     else
       @songs = Song.all
     end
   end
 
-  def show # rubocop:disable Metrics/AbcSize
+  def show
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
-      if @song.nil?
-        redirect_to artist_songs_path(@artist), alert: "Song not found"
-      end
+      song_check
     else
       @song = Song.find(params[:id])
     end
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -41,7 +39,11 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      check_song_and_artist
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -63,9 +65,35 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+  def artist_check
+    if @artist.nil?
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @songs = @artist.songs
+    end
+  end
+
+  def song_check
+    redirect_to redirect, alert: "Song not found" if @song.nil?
+  end
+
+  def check_song_and_artist
+    @artist = Artist.find_by(id: params[:artist_id])
+    if @artist.nil?
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = @artist.songs.find_by(id: params[:id])
+      song_check
+    end
+  end
+
+  def redirect
+    artist_songs_path(@artist)
+  end
+
 private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
